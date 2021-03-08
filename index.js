365f7e8959b62f67ed843162cc2eb4a4fc3cc7c9
neil <nvolungis@gmail.com>
2021-03-07 19:55:59 -0700

    remove old boilerplate, change port to 8000, use socket.io

--- index.js
+++ index.js
@@ -1,86 +1,21 @@
-const express = require('express')
-const WebSocket = require('ws')
-const routes = require('./routes')
-const Methods = require('./methods')
-const { generateError, normalizePort, safeParseJSON } = require('./helpers')
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  }
+});
 
-const app = express()
+io.on('connection', (socket) => {
+  console.log('a user connected');
 
-Object.keys(routes).forEach(key => {
-  app.use(`/${key}`, routes[key])
-})
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
-app.use((req, res) => {
-  return res.status(404).json(
-    generateError({
-      error: 'Not Found',
-      reasons: [
-        {
-          reason: 'invalid_path',
-          message: 'The requested path could not be found',
-          data: req.path,
-          location: 'path'
-        }
-      ]
-    })
-  )
-})
-
-const server = app.listen(process.env.WEB_PORT || 3000, () =>
-  console.log(
-    `Express Server is now listening on PORT: ${server.address().port}`
-  )
-)
-
-
-
-const WSS = new WebSocket.Server({ server })
-
-WSS.on('listening', () => {
-  console.log(
-    `WebSocket Server is now listening on PORT: ${WSS.address().port}`
-  )
-})
-
-WSS.on('connection', ws => {
-  ws.on('message', message => {
-    const data = safeParseJSON(message)
-
-    if (data === null) {
-      ws.send(
-        JSON.stringify(
-          generateError({
-            error: 'Parse Error',
-            reasons: [
-              {
-                reason: 'invalid_message_data',
-                message: 'Unable to parse the message contents',
-                data: message,
-                location: 'websocket-message'
-              }
-            ]
-          })
-        )
-      )
-    } else if (typeof data.method === 'string' && Methods[data.method]) {
-      Methods[data.method](WSS, ws, data)
-    } else {
-      ws.send(
-        JSON.stringify(
-          generateError({
-            error: 'Method Not Found',
-            reasons: [
-              {
-                reason: 'invalid_method',
-                message: 'Unable to find matching method',
-                data: data.method,
-                location: 'method'
-              }
-            ]
-          })
-        )
-      )
-    }
-  })
-})
-
+http.listen(8000, () => {
+  console.log('listening on *:8000');
+});
